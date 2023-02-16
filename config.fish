2d30c434f0d8bf4b71e227701ece8868ec1523ca
Robin Janzen <rjjanzen@web.de>
2023-02-16 07:51:23 +0100

    feat: init config.fish

---
+++ config.fish
@@ -0,0 +1,25 @@
+# let fish_greeting "Let's go.."
+# theme
+
+# aliases
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+end
+alias g git
+alias lg lazygit
+command -qv nvim && alias vim /opt/homebrew/opt/neovim/bin/nvim
+alias todo "cd ~/Dropbox/org/ && vim agenda.org"
+
+set -gx EDITOR nvim
+
+function fish_greeting
+  neofetch
+end
+
+set -g theme_color_scheme nord
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+# starship init fish | source
