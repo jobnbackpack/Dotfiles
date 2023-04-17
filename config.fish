a93f869846f0f8b4f18b31523e0e87d12687d032
Robin Janzen <rjanzen@conet.de>
2023-04-17 09:20:41 +0200

    feat: alt as prefix key + fish stuff

--- config.fish
+++ config.fish
@@ -6,18 +6,18 @@ if type -q exa
   alias ll "exa -l -g --icons"
   alias lla "ll -a"
 end
-# for wsl only
-# alias git git.exe
 alias g git
 alias lg lazygit
-alias tx tmuxifier
-command -qv nvim && alias vim /opt/homebrew/opt/neovim/bin/nvim
+# command -qv nvim && alias vim /opt/homebrew/opt/neovim/bin/nvim
+command -qv nvim && alias vim nvim
+zoxide init fish | source
+
 alias todo "cd ~/Dropbox/org/ && vim agenda.org"
 
 set -gx EDITOR nvim
 
 function fish_greeting
-  neofetch
+  # neofetch
 end
 
 set -g theme_color_scheme zenburn
@@ -25,7 +25,18 @@ set -g theme_color_scheme zenburn
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 # starship init fish | source
 set -gx PATH "$HOME/.tmux/plugins/tmuxifier/bin" $PATH
 eval (tmuxifier init - fish)
-alias tx tmuxifier
+alias tx 'tmuxifier load-session'
+
+# display path var for WSL rendering in windows
+export DISPLAY=:0
+# java home WSL
+set --export JAVA_HOME /opt/jdk-17.0.2
+set --export M2_HOME /opt/maven
+set --export RUST_HOME /home/rjanzen/.cargo/bin
+set --export NVM_HOME /home/rjanzen/.nvm
+set --export NODE_HOME /home/rjanzen/.nvm/versions/node/v19.9.0/bin
+set -gx PATH $NODE_HOME $NVIM_HOME $RUST_HOME $NVM_HOME $M2_HOME $JAVA_HOME $PATH
