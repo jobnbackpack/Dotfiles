acc9cc6a7d6d1f7eea16a7b44f7ed56be26a45cf
Robin Janzen <rjanzen@conet.de>
2024-06-07 12:50:47 +0200

    fix: supermaven with capital Y

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -26,7 +26,7 @@ return {
     config = function()
       require("supermaven-nvim").setup({
         keymaps = {
-          accept_suggestion = "<C-y>",
+          accept_suggestion = "<C-Y>",
           clear_suggestion = "<C-]>",
           accept_word = "<C-j>",
         },
