b57c9f17362970eedbdd450c609e7dc81a5a0654
Robin Janzen <rjanzen@conet.de>
2025-01-13 07:24:59 +0100

    feat: deno ls and ts

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+local nvim_lsp = require("lspconfig")
 return {
   {
     "neovim/nvim-lspconfig",
@@ -5,6 +6,24 @@ return {
       inlay_hints = { enabled = false },
       servers = {
         astro = {},
+        denols = {
+          filetypes = { "typescript", "typescriptreact" },
+          root_dir = function(...)
+            return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...)
+          end,
+        },
+        vtsls = {
+          root_dir = function()
+            return not vim.fs.root(0, { "deno.json", "deno.jsonc" })
+              and vim.fs.root(0, {
+                "tsconfig.json",
+                "jsconfig.json",
+                "package.json",
+                ".git",
+              })
+          end,
+          single_file_support = false,
+        },
       },
     },
   },
